e07531bad7d0dc70da0e52e78f002fb3a1e8bbd9
Ramachandra Reddy <ramachandra33@hotmail.com>
2024-06-26 21:49:59 +0530

    first commit

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -58,7 +58,7 @@ const Search = () => {
           id="price"
           class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="serach here..."
-          style={{ width: "40%" }}
+          style={{ width: "40%", border: "1px solid orange" }}
           onChange={(e) => handleSearch(e.target.value)}
         />
         <ul role="list" class="divide-y divide-gray-100">
